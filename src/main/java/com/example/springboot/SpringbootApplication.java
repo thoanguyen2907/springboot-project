9642ff55536f50c55518de0f10ee02b902ddf199
thoanguyen2907 <thoanguyen290795@gmail.com>
2022-06-15 22:15:27 +0300

    apply FeignClient

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -3,11 +3,12 @@ package com.example.springboot;
 import com.example.springboot.customer.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@EnableFeignClients
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
