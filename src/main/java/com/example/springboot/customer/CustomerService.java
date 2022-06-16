a43af262b2246486c50f4d010473f416bde35aec
thoanguyen2907 <thoanguyen290795@gmail.com>
2022-06-16 22:37:24 +0300

    add Logger

--- src/main/java/com/example/springboot/customer/CustomerService.java
+++ src/main/java/com/example/springboot/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.springboot.customer;
 
 import com.example.springboot.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import java.util.List;
 @Service
 
 public class CustomerService {
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
     private final CustomerRepository customerRepository;
     @Autowired
     public CustomerService(CustomerRepository customerRepository) {
