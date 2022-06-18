144b7fd8e9a9f7b4d52dc0cc779cf3d24e4a77db
thoanguyen2907 <thoanguyen290795@gmail.com>
2022-06-18 21:35:54 +0300

    add dev environment

--- src/main/java/com/example/springboot/customer/CustomerConfiguration.java
+++ src/main/java/com/example/springboot/customer/CustomerConfiguration.java
@@ -1,18 +1,31 @@
 package com.example.springboot.customer;
 
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class CustomerConfiguration {
     @Value("${app.useFakeCustomerRepo: false}")
     private Boolean useFakeCustomerRepo;
+
+    @Value("${info.company.name}")
+    private  String companyName;
+
+   // @Autowired
+    //private Environment environment;
+
     @Bean
     CommandLineRunner commandLineRunner() {
         return args -> {
             System.out.println("Command Line runner !!!!");
+            System.out.println(companyName);
+           // System.out.println(environment.getProperty("info.app.version"));
+
         };
     }
     @Bean
